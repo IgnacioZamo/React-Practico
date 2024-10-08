088a1bba5964b502cdafb4ea38cc91aabdffebd2
Nacho Netbook <ignaciozamo1@gmail.com>
2024-10-07 22:10:29 -0300

    se agrega ejercicio de formulario con formik, a completar

---
+++ 07-formulariocoContacto/src/App.jsx
@@ -0,0 +1,12 @@
+import AppForm from "./assets/form/AppForm"
+
+
+function App() {
+  return (
+    <>
+      <AppForm/>
+    </>
+  )
+}
+
+export default App
---
+++ 07-formulariocoContacto/src/assets/form/AppForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppForm = () => {
+  return (
+    <div><p>holaaa</p></div>
+  )
+}
+
+export default AppForm
