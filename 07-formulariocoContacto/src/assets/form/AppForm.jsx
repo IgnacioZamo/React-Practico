4e6f1625bd4f7c3dfc196bc18c27e22c4e57cef8
Ignacio Z <ignaciozamo1@gmail.com>
2024-10-09 00:35:44 -0300

    Se practica la creacion de formularios mediante formik, en especial: initialValues, Validate, ErrorMessage

--- 07-formulariocoContacto/src/assets/form/AppForm.jsx
+++ 07-formulariocoContacto/src/assets/form/AppForm.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Form, Formik, Field, ErrorMessage } from 'formik'
 
 const AppForm = () => {
   return (
-    <div><p>holaaa</p></div>
+    <Formik
+    initialValues={{message:"Ingrese su mensaje"}}
+    validate ={values =>{
+      let errors = {};
+      if(!values.name){
+        errors.name="Este campo es requerido";
+        } else if (!values.email){
+          errors.email = "Este campo es requerido";
+        } else if(!values.message){
+          errors.message="Este campo es requerido"
+        }
+        return errors;
+      }
+    }
+    >
+      {
+        ({isSubmiting, values})=>(
+          <Form>
+            <div>
+              <label htmlFor="name">Nombre:</label>
+              <Field type="text" name="name"></Field>
+              <ErrorMessage name="name" component="p"/>
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <Field type="text" name="email"></Field>
+              <ErrorMessage name="email" component="p"/>
+            </div>
+            <div>
+              <label htmlFor="message">Comentario:</label>
+              <Field component="textarea" values={values.message} name="message"></Field>
+              <ErrorMessage name="message" component="p"/>
+            </div>
+          </Form>
+        )
+      }
+
+    </Formik>
   )
 }
 
 export default AppForm
+
+
+
+//se instaló la dependencia formik
